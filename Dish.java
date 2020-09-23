5a904cfa5f4c3e999fda42bf3cb584ea26f9bcc8
Yushachkov Anton <71340057+Pro100kak2s@users.noreply.github.com>
2020-09-23 22:05:59 +0300

    Add files via upload

---
+++ Dish.java
@@ -0,0 +1,17 @@
+public abstract class Dish {
+    protected double price;
+    protected String color;
+    protected String fall;
+
+    public abstract void SFall();
+
+    public void SetColor(String color)
+    {
+        this.color = color;
+
+    }
+    public void SetPrice( double price)
+    {
+        this.price = price;
+    }
+}
